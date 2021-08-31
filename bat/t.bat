6484f7284a0a88674c797c55db5e1cb42dcc9d3c
AKitavtse <AKitavtse@mail.ru>
2021-08-31 17:56:01 +0300

    revue

--- bat/t.bat
+++
@@ -1,1 +0,0 @@
-curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde045264?title=Paul%20M&tag=6"
--- bat/where-order.bat
+++ bat/where-order.bat
@@ -1,4 +1,4 @@
-curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde045264?title=Paul&tag=6"
+curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde045264?tag=6&title=Paul%%20M"
 
 
 curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde045264?tag=6"
@@ -11,5 +11,5 @@ curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde
 curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde045264?created_gt=2021-08-03"
 
 curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde045264?find=Paul
-curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde045264?tags_in=\[1,2\]&title=Paul%20M&created_gt=2021-07-10&name=Bred&category=5&find=Pau&text=McC"
+curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde045264?tags_in=\[1,2\]&title=Paul&created_gt=2021-07-10&name=Bred&category=5&find=Pau&text=McC"
 curl -v "http://localhost:3000/posts/1.120210901202553ff034f3847c1d22f091dde7cde045264?order=\[photo,date,author,category\]"
